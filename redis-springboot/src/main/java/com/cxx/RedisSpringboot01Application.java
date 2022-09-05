c2c7712dc920f0f2793da57c721ac1cc71769b02
cxx2002 <244665429@qq.com>
2022-09-05 09:55:58 +0800

    first commit

---
+++ redis-springboot/src/main/java/com/cxx/RedisSpringboot01Application.java
@@ -0,0 +1,13 @@
+package com.cxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringboot01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSpringboot01Application.class, args);
+    }
+
+}
---
+++ redis-springboot/src/main/java/com/cxx/controller/RedisController.java
@@ -0,0 +1,31 @@
+package com.cxx.controller;
+
+/**
+ * @author 陈喜喜
+ * @date 2022-08-11 11:31
+ * springboot 没有选择web功能
+ */
+/*@Slf4j
+@RequestMapping("/redis")
+@RestController
+public class RedisController {
+
+    @Resource
+    private RedisUtil redisUtil;
+
+    @RequestMapping("set")
+    public boolean redisset(String key, String value){
+        return redisUtil.set(key,value);
+    }
+
+    @RequestMapping("get")
+    public Object redisget(String key){
+        return redisUtil.get(key);
+    }
+
+    @RequestMapping("expire")
+    public boolean expire(String key){
+        return redisUtil.expire(key,ExpireTime);
+    }
+
+}*/
